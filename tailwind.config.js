95c1dad6f06a0b701d5f5ec6f9f498e93e20fb67
NjokiGithukuE <njokigithuku20@gmail.com>
2024-12-18 16:34:48 +0300

    dark mode removed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-export default {content: [
+export default {
+    darkMode: 'false',
+    content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
